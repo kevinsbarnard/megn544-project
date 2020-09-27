cfe72b17053b420fa970605cdeca2d56b53a47ea
Kevin Barnard <kbarnard@mbari.org>
2020-09-27 13:30:36 -0600

    Added rot2AngleAxis and rot2Quat

--- rot2AngleAxis.m
+++ rot2AngleAxis.m
@@ -1,7 +1,33 @@
-function [outputArg1,outputArg2] = rot2AngleAxis(inputArg1,inputArg2)
-%ROT2ANGLEAXIS Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+% rot2AngleAxis Convert a rotation matrix to an angle-axis rotation
+%
+%   [k, theta] = rot2AngleAxis(R)
+%
+%   k = rotation axis (unit 3x1 vector)
+%   theta = angle (rad)
+%
+%   R = rotation matrix (3x3 matrix)
+%
+%   Kevin Barnard
+%   10827168
+%   MEGN 544
+%   9/7/2020
 
+function [k, theta] = rot2AngleAxis(R)
+    r1 = R(1,1);
+    r2 = R(2,2);
+    r3 = R(3,3);
+
+    cos_theta = (r1 + r2 + r3 - 1) / 2;
+    axis_vec = [R(3,2)-R(2,3) R(1,3)-R(3,1) R(2,1)-R(1,2)]';
+    theta = atan2(norm(axis_vec)/2, cos_theta);
+    
+    if abs(theta - pi) < 1e-12
+        S = (R + eye(3)) / 2;
+        k1 = sqrt(S(1,1));
+        k2 = S(1,2)/k1;
+        k3 = S(1,3)/k1;
+        k = [k1 k2 k3]';
+    else
+        k = axis_vec ./ (2 * sin(theta));
+    end
+end
--- rot2Quat.m
+++ rot2Quat.m
@@ -1,7 +1,43 @@
-function [outputArg1,outputArg2] = rot2Quat(inputArg1,inputArg2)
-%ROT2QUAT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+% rot2Quat Convert a rotation matrix to a quaternion
+%
+%   [q] = rot2Quat(R)
+%
+%   q = quaternion (4x1 vector)
+%
+%   R = rotation matrix (3x3 matrix)
+%
+%   Kevin Barnard
+%   10827168
+%   MEGN 544
+%   9/7/2020
 
+function [q] = rot2Quat(R)
+    r1 = R(1,1);
+    r2 = R(2,2);
+    r3 = R(3,3);
+    tr = r1 + r2 + r3;
+
+    if tr > eps
+        q0 = sqrt((1 + tr) / 4);
+        q1 = (R(3,2) - R(2,3)) / 4;
+        q2 = (R(1,3) - R(3,1)) / 4;
+        q3 = (R(2,1) - R(1,2)) / 4;
+    elseif r1 > r2 && r1 > r3
+        q1 = sqrt((1 + r1 - r2 - r3) / 4);
+        q0 = (R(3,2) - R(2,3))/(4*q1);
+        q2 = (R(1,2) + R(2,1))/(4*q1);
+        q3 = (R(1,3) + R(3,1))/(4*q1);
+    elseif r2 > r1 && r2 > r3
+        q2 = sqrt((1 - r1 + r2 - r3) / 4);
+        q0 = (R(1,3) - R(3,1))/(4*q2);
+        q1 = (R(1,2) + R(2,1))/(4*q2);
+        q3 = (R(2,3) + R(3,2))/(4*q2);
+    else
+        q3 = sqrt((1 - r1 - r2 + r3) / 4);
+        q0 = (R(2,1) - R(1,2))/(4*q3);
+        q1 = (R(1,3) + R(3,1))/(4*q3);
+        q2 = (R(2,3) + R(3,2))/(4*q3);
+    end
+    
+    q = [q0 q1 q2 q3]';
+end
